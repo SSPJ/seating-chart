84fc16e8f3c63cddf71f19e5ecf242674ae162a6
Seamus <git@seamusjohnston.com>
2018-01-19 04:12:32 -0500

    Tests pass on example input

--- seating_chart.py
+++ seating_chart.py
@@ -1,28 +1,99 @@
+import re
+
 class SeatingChart:
+
+  _RESERVE_DATA_TYPE_ERROR = """"Reservation must be integer or string"""
+
   def __init__(self,rows,columns):
-    self.rows = rows
-    self.cols = columns
+    self._rows = rows
+    self._cols = columns
+    self._reserved_seats = {}
+    self._sold_out = False
+
+  def initialize(self, res_lst):
+    for res in res_lst:
+      self._individual(res)
+
+  def _get_key(self,r,c):
+    """ Hash function. """
+    return c * 1000 + r
+
+  # def _parse_key(self,key):
+  #   """ Undo hash function. """
+  #   c = int(key / 1000)
+  #   r = key - ( c * 1000 )
+  #   return r,c
+
+  def _get_taxi(self,seat):
+    """ Return the Manhattan Distance from stage center of a given seat. """
+    r,c = seat
+    middle = self._cols / 2 + .5 if self._cols % 2 == 0 else self._cols / 2 + 1
+    taxi = int(round(abs(c - middle) + abs(r - 1)))
+    return taxi
 
   def _individual(self,seat):
-    pass
+    """ Reserve seat in chart if not already reserved. """
+    key = self._get_key(*seat)
+    if key not in self._reserved_seats:
+      self._reserved_seats[key] = 1
+      return [seat]
+    else:
+      return None
 
   def _find_seats(self,sought):
-    # sudo code 
-# check the first row
-#   if #seats found, record the taxicab distance & $seats
-#     check subsequent rows within the taxicab distance for better seats
-#     if better found, update $seats and taxicab distance
-# if no seats found, check first row + 1
-    pass
+
+    if self._cols < sought or self._sold_out:
+      return None
+
+    '''Traverse each row, stage to rear. For each row,
+    seek all blocks of unreserved consecutive seats == number sought'''
+    candidates = {}
+    for row in range(1,self._rows+1):
+      seats_to_reserve = []
+      for col in range(1,self._cols+1):
+        seat = (row,col)
+        # Do not take someone else's seat
+        if self.reserved(seat):
+          seats_to_reserve = []
+        else:
+          seats_to_reserve.append(seat)
+        # Have we found enough seats?
+        if len(seats_to_reserve) == sought:
+          # Measure how far from stage center
+          taxi_left = self._get_taxi(seats_to_reserve[0])
+          taxi_right = self._get_taxi(seats_to_reserve[-1])
+          taxi_dist = taxi_right if taxi_right >= taxi_left else taxi_left
+          # Record these seats for later
+          if taxi_dist not in candidates:
+            candidates[taxi_dist] = list(seats_to_reserve)
+          # Make len(seats_to_reserve) != sought
+          del seats_to_reserve[0]
+
+    if candidates:
+      reservation_list = candidates[min(candidates,key=int)]
+      for res in reservation_list:
+        # NB: if the SeatingChart object is presisted to a database,
+        # these reservations must be inside a transaction
+        self._individual(res)
+      return (reservation_list[0],reservation_list[-1])
+    else:
+      return None
 
   def reserve(self,seats):
-    if isinstance(seats,str):
-      reservations = _individual(seats)
+    """ Reserve one seat or attempt to find reservations for a given number.
+
+    Returns None if reservation fails otherwise
+    Returns the reserved seats as a sorted list
+    """ 
+    if isinstance(seats,tuple):
+      reservations = self._individual(seats)
     elif isinstance(seats,int):
-      reservations = _find_seats(seats)
+      reservations = self._find_seats(seats)
     else:
-      raise AttributeError("Reservation must be integer or string")
+      raise ValueError(self._RESERVE_DATA_TYPE_ERROR)
     return reservations
 
   def reserved(self,seat):
-    pass
+    """ Is seat resevered? """
+    key = self._get_key(*seat)
+    return True if key in self._reserved_seats else False
--- seating_chart_test.py
+++ seating_chart_test.py
@@ -1,10 +1,146 @@
-import unittest
-
+import pytest
+import re
 from seating_chart import SeatingChart
 
-class SeatingChartTests(unittest.TestCase):
+class TestSeatingChart():
 
+### Helper ###
+  # def populate(self,chart,res):
+  #   res = res.split()
+  #   for x in res:
+  #     r,c = re.match(r"\s*[Rr]([0-9]+)[Cc]([0-9]+)\s*",x).group(1,2)
+  #     seat = (int(r),int(c))
+  #     chart.reserve(seat)
+
+### class SeatingChart ###
+  def test_create_new_seating_chart(self):
+    chart = SeatingChart(10,11)
+    assert isinstance(chart,SeatingChart)
+    assert chart._rows == 10
+    assert chart._cols == 11
+    assert chart._reserved_seats == {}
+    assert chart._sold_out == False
+
+### def _get_key(self,r,c) ###
+  def test_cant_change_hash_formula_by_accident(self):
+    chart = SeatingChart(10,11)
+    assert chart._get_key(12,23) == 23012
+
+### def _parse_key(self,key) ###
+  # def test_get_seat(self):
+  #   chart = SeatingChart(10,11)
+  #   assert chart._parse_key(23012) == (12,23)
+
+### def _get_taxi(self,a,b) ###
+  def test_get_taxi_odd_cols(self):
+    chart = SeatingChart(5,9)
+    left = chart._get_taxi((4,8))
+    right = chart._get_taxi((4,2))
+    assert left == right
+
+  def test_get_taxi_even_cols(self):
+    chart = SeatingChart(5,10)
+    left = chart._get_taxi((4,9))
+    right = chart._get_taxi((4,2))
+    assert left == right
+
+### def _individual(self,seat) ###
+  def test_reserving_a_seat_once(self):
+    chart = SeatingChart(5,5)
+    expected = [(2,8)]
+    actual = chart.reserve((2,8))
+    assert actual == expected
+
+  def test_reserving_a_seat_twice(self):
+    chart = SeatingChart(5,5)
+    chart.reserve((2,8))
+    expected = None
+    actual = chart.reserve((2,8))
+    assert actual == expected
+
+### def _find_seats(self,sought) ###
+  def test_group_bigger_than_a_row(self):
+    chart = SeatingChart(10,6)
+    expected = None
+    actual = chart._find_seats(7)
+    assert actual == expected
+
+  def test_find_best_four_seats_in_empty_row_of_odd_columns(self):
+    chart = SeatingChart(1,9)
+    chart._find_seats(4)
+    expected = {3001: 1, 4001: 1, 5001: 1, 6001: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+    
+  def test_find_best_four_seats_in_empty_row_of_even_columns(self):
+    chart = SeatingChart(1,10)
+    chart._find_seats(4)
+    expected = {4001: 1, 5001: 1, 6001: 1, 7001: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+
+  def test_find_best_five_seats_in_empty_row_of_odd_columns(self):
+    chart = SeatingChart(1,9)
+    chart._find_seats(5)
+    expected = {3001: 1, 4001: 1, 5001: 1, 6001: 1, 7001: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+
+  def test_find_best_five_seats_in_empty_row_of_even_columns(self):
+    chart = SeatingChart(1,10)
+    chart._find_seats(5)
+    expected = {3001: 1, 4001: 1, 5001: 1, 6001: 1, 7001: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+
+  def test_find_best_five_seats_in_second_row(self):
+    chart = SeatingChart(5,10)
+    chart.initialize([(1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8),
+                     (1,9), (1,10)])
+    chart._find_seats(5)
+    expected = {4001: 1, 2001: 1, 1001: 1, 9001: 1, 7001: 1, 6001: 1, 5001: 1,
+                3001: 1, 10001: 1, 8001: 1, 3002: 1, 4002: 1, 5002: 1, 6002: 1,
+                7002: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+
+  def test_no_space_left_for_group(self):
+    chart = SeatingChart(2,6)
+    chart.initialize([(1,1), (1,3), (1,5), (2,2), (2,4), (2,6)])
+    expected = None
+    actual = chart._find_seats(2)
+    assert actual == expected
+
+  def test_example_input(self):
+    chart = SeatingChart(3,11)
+    chart.initialize([(1,4), (1,6), (2,3), (2,7), (3,9), (3,10)])
+    chart._find_seats(3)
+    chart._find_seats(3)
+    chart._find_seats(3)
+    chart._find_seats(1)
+    chart._find_seats(10)
+    expected = {3002: 1, 4001: 1, 4002: 1, 5001: 1, 5002: 1, 5003: 1, 6001: 1,
+                6002: 1, 6003: 1, 7001: 1, 7002: 1, 7003: 1, 8001: 1, 9001: 1,
+                9003: 1, 10003: 1}
+    actual = chart._reserved_seats
+    assert expected == actual
+
+### def reserve(self,seats) ###
   def test_reserve_takes_only_strs_or_ints(self):
     chart = SeatingChart(5,5)
-    with self.assertRaises(AttributeError):
+    with pytest.raises(ValueError):
       chart.reserve(4.5)
+
+### def reserved(self,seat) ###
+  def test_reserved_seat_is_marked_reserved(self):
+    chart = SeatingChart(5,5)
+    chart.reserve((1,3))
+    expected = True
+    actual = chart.reserved((1,3))
+    assert actual == expected
+
+  def test_unreserved_seat_is_marked_unreserved(self):
+    chart = SeatingChart(5,5)
+    expected = False
+    actual = chart.reserved((1,3))
+    assert actual == expected
