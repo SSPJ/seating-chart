62413f19a5aa983c8652fe8942bb5ebc50dd85fe
Seamus <git@seamusjohnston.com>
2018-01-19 23:19:12 -0500

    Refactor input for easier testing

--- driver.py
+++ driver.py
@@ -1,59 +1,73 @@
 #!/usr/bin/env python3
+# Seamus Johnston, 2018
 import re, sys
 
 from seating_chart import SeatingChart
 
-ROWS = 5      # people to your left and right
-COLUMNS = 6   # people behind and in front of you
+ROWS = 3       # people to your left and right
+COLUMNS = 11   # people behind and in front of you
 
 # Output strings grouped for easier extraction in future l18n
-INTIAL_INPUT_ERROR = """Inital reservations must be of the form 'R3C5 R2C8 R4C5'
-Enter them correctly or press Ctrl-D to abort."""
+INTIAL_INPUT_ERROR = """Inital reservations must be of the form 'R3C5 R2C8 R4C5'."""
 GROUP_RESERVATION_INPUT_ERROR = """Additional reservations must be the party number,
 entered numerically, one per line."""
+PARSE_SEAT_VALUE_ERROR = """Seat must be given as R#C#, eg 'R3C15'"""
 
-def initialize_reservations(chart, res_lst):
-  for res in res_lst:
-    chart.reserve(res)
+def parse_seat(seat):
+  """ Return a tuple (row, column) when given string in the form 'R#C#' """
+  r,c = re.match(r"\s*[Rr]([0-9]+)[Cc]([0-9]+)\s*",seat).group(1,2)
+  return int(r),int(c)
 
-def initial_reservation_input():
-  ir = input()
-  # If input contains invalid chars, enter an input loop
-  # NB: this won't stop silly input like 'RRCC 1216'
-  while re.search(r"[^RCrc0-9\s]+",ir):
+def parse_intial_string(ir):
+  """ Converts a string of reservations into a list of tuples.
+
+  "R1C3 R2C5 R3C6" -> [(1,3),(2,5),(3,6)]
+  """
+  rc_tuples = []
+  if re.search(r"[^RCrc0-9\s]+",ir):
     print(INTIAL_INPUT_ERROR)
-    ir = input()
-  return ir.split()
+    sys.exit(1)
+  for res in ir.split():
+    try:
+      rc_tuples.append(parse_seat(res))
+    except AttributeError:
+      print(PARSE_SEAT_VALUE_ERROR)
+      sys.exit(1)
+  return rc_tuples
 
-def additional_reservations(chart): 
-  while True:
-    number_of_seats = input()
-    if number_of_seats.isdigit():
-      print("Not Available")
-      seats_reserved = chart.reserve(int(number_of_seats))
-      if seats_reserved:
-        # TODO: print seats
-        pass
-    else:
-      print(GROUP_RESERVATION_INPUT_ERROR)
+def parse_output(res):
+  """ Convert a list of seat tuples into a formated string. """
+  if res[0][1] == res[1][1]:
+    return "R{0}C{1}".format(res[0][0],res[0][1])
+  else:
+    return "R{0}C{1} - R{2}C{3}".format(res[0][0],res[0][1],res[1][0],res[1][1])
+
+def additional_reservation(chart,n_seats):
+  """ Reserve a group of seats or return None. """
+  if n_seats.isdigit():
+    return chart.reserve(int(n_seats))
+  else:
+    print(GROUP_RESERVATION_INPUT_ERROR)
+    return None
 
 if __name__ == "__main__":
   ### Create the SeatingChart ojbect ###
   chart = SeatingChart(ROWS,COLUMNS)
 
   ### Get initial reservations ###
-  try:
-    reservation_list = initial_reservation_input()
-  except EOFError:
-    # NB: if the SeatingChart object is presisted outside of memory,
-    # eg. written to a database, it must be deleted here
-    sys.exit()
-
+  initial_reservations = input()
+  reservation_list = parse_intial_string(initial_reservations)
   if reservation_list:
-    initialize_reservations(chart,reservation_list)
+    chart.initialize(reservation_list)
 
   ### Obtain further reservations ###
   try:
-    additional_reservations(chart)
+    while True:
+      number_of_seats = input()
+      seats_reserved = additional_reservation(chart,number_of_seats)
+      if seats_reserved:
+        print(parse_output(seats_reserved))
+      else:
+        print("Not Available")
   except EOFError:
-    pass
+    print(chart.number_available)
--- driver_test.py
+++ driver_test.py
@@ -1,3 +1,4 @@
+# Seamus Johnston, 2018
 import pytest
 
 import driver
@@ -5,25 +6,40 @@ from seating_chart import SeatingChart
 
 class TestDriver():
 
-  ### def initialize_reservations(chart, res_lst) ###
+  ### def parse_seat(seat) ###
+  def test_parse_seat_given_nonsense(self):
+    with pytest.raises(AttributeError):
+      driver.parse_seat("r\4c76")
 
-  ### def initial_reservation_input() ###
-  @pytest.mark.skip(reason="need to implement a generator for the lambda")
-  def test_initial_reservation_input_handles_bad_chars(self,monkeypatch):
-    monkeypatch.setitem(__builtins__, 'input', lambda: raise EOFError)
-    driver.initial_reservation_input()
-
-  def test_intial_reservation_input_parses_known_good_vals(self,monkeypatch):
-    monkeypatch.setitem(__builtins__, 'input', lambda: "R3c5 R2C7    r1C4")
-    expected = ["R3c5","R2C7","r1C4"]
-    actual = driver.initial_reservation_input()
+  def test_parse_seat_given_correct_input(self):
+    expected = (5,83)
+    actual = driver.parse_seat("R5c83   ")
     assert actual == expected
 
-  ### def additional_reservations(chart) ###
-  @pytest.mark.skip(reason="need to implement a generator for the lambda")
-  def test_row_not_wide_enough(self,monkeypatch,capsys):
-    monkeypatch.setitem(__builtins__, 'input', lambda: "10")
+  ### def parse_intial_string(ir) ###
+  def test_initial_string_with_bad_input(self):
+    with pytest.raises(SystemExit):
+      driver.parse_intial_string("R2C5\4")
+
+  def test_initial_input_can_be_flexible(self):
+    expected = [(3,5),(2,7),(1,4)]
+    actual = driver.parse_intial_string("R3c5 R2C7    r1C4")
+    assert actual == expected
+
+  ### def parse_output(res) ###
+  def test_parse_output_with_three_seats_reserved(self):
+    expected = "R1C7 - R1C9"
+    actual = driver.parse_output([(1,7),(1,9)])
+    assert actual == expected
+
+  def test_parse_output_with_one_seat_reserved(self):
+    expected = "R1C5"
+    actual = driver.parse_output([(1,5),(1,5)])
+    assert actual == expected
+
+  ### def additional_reservations(chart,n_seats) ###
+  def test_row_not_wide_enough(self):
     chart = SeatingChart(5,5)
-    expected = "Not Available"
-    actual = capsys.readouterr().out
+    expected = None
+    actual = driver.additional_reservation(chart,"10")
     assert actual == expected
